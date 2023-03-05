bf95ee9f46c8d47cf571d1888e1f70880b20fa53
Rahul Parkar <rahul.a.parkar@gmail.com>
2023-03-05 18:53:04 +0000

    Add GA Tracking ID

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import type { MetaFunction, LinksFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -6,9 +8,13 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  useLocation,
 } from "@remix-run/react";
 
 import styles from "./tailwind.css";
+
+import * as gtag from "~/utils/gtags.client";
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
@@ -18,7 +24,21 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
+// Load the GA tracking id from the .env
+export const loader = async () => {
+  return json({ gaTrackingId: process.env.GA_TRACKING_ID });
+};
+
 export default function App() {
+  const location = useLocation();
+  const { gaTrackingId } = useLoaderData<typeof loader>();
+
+  useEffect(() => {
+    if (gaTrackingId?.length) {
+      gtag.pageview(location.pathname, gaTrackingId);
+    }
+  }, [location, gaTrackingId]);
+
   return (
     <html lang="en">
       <head>
@@ -26,6 +46,29 @@ export default function App() {
         <Links />
       </head>
       <body>
+        {!gaTrackingId ? null : (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
+            />
+            <script
+              async
+              id="gtag-init"
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${gaTrackingId}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+              }}
+            />
+          </>
+        )}
+
         <Outlet />
         <ScrollRestoration />
         <Scripts />
